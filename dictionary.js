86fc3c65e82a972eeb75337618dafb38c02a5584
Zachbins <quezantonio54@outlook.com>
2026-05-01 23:53:22 +0800

    dd

---
+++ dictionary.js
@@ -0,0 +1,7 @@
+const dictionaryList = [
+    { word: "Würz", meaning: "Root / Origin", pos: "noun", pronunciation: "/vʏrts/" },
+    { word: "Grevein", meaning: "Language / Tongue", pos: "noun", pronunciation: "/ˈɡreɪvaɪn/" },
+    { word: "Hallo", meaning: "Hello", pos: "interjection", pronunciation: "/ˈhalo/" },
+    { word: "Welt", meaning: "World", pos: "noun", pronunciation: "/vɛlt/" },
+    { word: "Buch", meaning: "Book", pos: "noun", pronunciation: "/buːx/" }
+];
